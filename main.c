dd46c1e06db2c9b628f72f559114e4a79adc3156
Rodrigo Moreno <bigomc@gmail.com>
2017-04-05 19:13:38 -0500

    First commit

---
+++ main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include "other.h"
+
+void fileCheck(const char *fileName);
+
+int main (void) {
+
+	/*verifyDeviceTree();
+
+	if(pciDevices()) {
+		printf("Valid NIC\n");;		
+	} else {
+		printf("Didn't find a valid NIC\n");
+	}*/
+
+	publicFunction();
+
+	return 0;
+}
